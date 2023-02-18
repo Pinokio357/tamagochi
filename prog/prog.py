c7024f7885b18d63da6340786b3e0132ffbafdde
ivanH <nigga.2023@mail.ru>
2023-02-18 11:07:05 +0300

    add class house

--- prog/prog.py
+++ prog/prog.py
@@ -4,12 +4,17 @@ class Cat():
         self.name = name
         self.power = 50
         self.age = 0
+        self. house = None
+    def go_into_house(self,house):
+        self.house = house
+        self.power -= 30
     def play(self):
         print(f"{self.name} play a ball")
         self.power -= 10
     def eat(self):
         print(f"{self.name} eat a meal")
         self.power += 20
+        self.house.food -= 10
     def sleep(self):
         print(f"{self.name} sleep all night")
         self.power += 50
@@ -18,9 +23,19 @@ class Cat():
         print(f"{self.name} walk down the street")
         self.power -= 15
     def __str__(self):
-        return f"i'm cat, my name is: {self.name}, my life force is: {self.power}, my age is: {self.age} days"
+        return f"i'm cat, my name is: {self.name},i live in house: {self.house}, my life force is: {self.power}, my age is: {self.age} days"
 
+class House():
+    def __init__(self, name):
+        self.name = name
+        self.food = 100
+
+    def __str__(self):
+        return f"house name is: {self.name}"
+
+my_house = House
 barsik = Cat("Barsik")
+barsik.go_into_house("my_house")
 for day in range(7):
     print(f"======================= {day} =========================")
     cube = randint(1, 6)
